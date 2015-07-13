66a25ae6d2e51fa1e4c9577c4398cc2caf517c97
Artem Kachugin <setterhead@gmail.com>
2015-07-13 16:52:38 +0400

    Update plot4.R

--- plot4.R
+++ plot4.R
@@ -1,5 +1,8 @@
 ## file household_power_consumption.txt must be in your working directory 
 Sys.setlocale("LC_TIME", "English")
+fileUrl <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileUrl, destfile = "./exdata_data_household_power_consumption.zip", method = "curl")
+unzip("./exdata_data_household_power_consumption.zip", exdir = ".")
 hpc <- read.csv("household_power_consumption.txt", header=T, sep=';', na.strings="?",nrows=2075259, check.names=F, stringsAsFactors=F, comment.char="", quote='')
 hpc$Date <- as.Date(hpc$Date, format="%d/%m/%Y")
 hpc2 <- subset(hpc, subset=(Date >= "2007-02-01" & Date <= "2007-02-02"))
